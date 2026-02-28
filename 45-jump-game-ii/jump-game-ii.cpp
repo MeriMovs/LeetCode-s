68abd92ebcb51122d7b8841d3d58f2931036f6c2
Meri Movsesyan <92723395+MeriMovs@users.noreply.github.com>
2026-02-28 17:12:22 +0400

    Time: 57 ms (21.46%) | Memory: 21.6 MB (16.04%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, INT_MAX);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (dp[i] == INT_MAX) continue;
+            for (int j = 1; j <= nums[i]; ++j) {
+                if(i+j >= n) break;
+                dp[i + j] = min(dp[i + j], dp[i] + 1);
+            }
+        }
+
+        return dp[n - 1];
+    }
+};
