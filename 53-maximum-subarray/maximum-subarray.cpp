a3a951dff5434779f5d2ffc520a3bc393b8ec06d
Meri Movsesyan <92723395+MeriMovs@users.noreply.github.com>
2026-03-31 22:48:15 +0400

    Time: 3 ms (19.60%) | Memory: 79.9 MB (14.84%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(2, vector<int>(n, 0));
+        dp[0][n-1] = nums[n-1];
+        dp[1][n-1] = nums[n-1];
+
+        for (int i = n-2; i >= 0; --i) {
+            dp[0][i] = nums[i] > nums[i] + dp[0][i+1] ? nums[i] : nums[i] + dp[0][i+1];
+        }
+
+        for (int i = n-2; i >= 0; --i) {
+            dp[1][i] = max(dp[0][i+1], dp[1][i+1]);
+        }
+
+        return max(dp[0][0], dp[1][0]);
+    }
+};
